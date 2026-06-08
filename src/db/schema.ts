a197d474b687d150e260d074af4c5331cd638ad0
MrEkwunife <ekwunifechimobi16@gmail.com>
2026-06-08 18:41:05 +0100

    db: users schema

---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, timestamp, uuid, text } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .notNull()
+    .$onUpdate(() => new Date()),
+  name: text("name").notNull().unique(),
+});
